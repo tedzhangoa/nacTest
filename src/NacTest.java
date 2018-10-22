46e3b8a8708de0df85fd417b9c765a3117c9c36e
TedZhang <ted.zhang@oa.com.au>
2018-10-22 14:43:44 +1100

    Created seperate function for refreshing device states
    Added PA playback test - not yet working

--- src/NacTest.java
+++ src/NacTest.java
@@ -1,7 +1,5 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
 import oa.as.*;
 
 class NacTest 
@@ -102,18 +100,20 @@ class NacTest
 		 
 		
 		
-		//Possible setup of device
+	//Possible setup of device
 		
 		//1. test dictionary update
 			//initiate update through CXS
 			//check resultant dictionary version on device
+		
+		//2. test dictionary changes on single item
 			
 		
 		//System.out.println("CXS dictionary version is: " + as.getDictionaryChangeset().isEmpty());
 		
 		
 		
-		//Check device attributes - pre-set attributes
+	//Check device attributes - pre-set attributes
 		System.out.println("Checking device attributes");
 		assertEquals(CURR_NAC_LOC_ID,d.getLocationId()); 
 		assertEquals(CURR_NAC_DEV_INDEX, d.getDeviceIndex());
@@ -123,13 +123,13 @@ class NacTest
 		String dest = Integer.toString(d.getDstNo());	
 		assertEquals("2"+ CURR_NAC_LOC_ID + CURR_NAC_DEV_INDEX, Integer.toString(d.getDstNo()));
 		
-		//Check device attributes - against CXS
+	//Check device attributes - against CXS
 		assertEquals(c.getDictionaryVersion(), d.getDictionaryVersion());
-		assertEquals(c.getSoftwareRevision(), d.getSoftwareRevision());
+		//assertEquals(c.getSoftwareRevision(), d.getSoftwareRevision());
 		
 		
-		//Check Device Model class 
-		//NOT FUNCTIONING
+	//Check Device Model class 
+		
 		System.out.println("Checking device input and output");			
 		System.out.println("NAC model name is: " + m.getName());
 		System.out.println("NAC digital inputs: " + m.getDigitalInputCount());
@@ -138,7 +138,7 @@ class NacTest
 		
 		
 		
-		//Check device Digital input/output setting/getting
+	//Check device Digital input/output setting/getting
 
 		//Digital inputs
 		for(int i=1; i<= m.getDigitalInputCount(); i++) {
@@ -153,26 +153,8 @@ class NacTest
 			catch (InterruptedException e) { }
 		}
 		//refresh device states 
+		d = refreshDevice(as); 
 
-		while(true) 
-		{
-			devices = as.getDeviceStates();
-			if (devices.size() > 0)// && !devices.get(0).getDeviceModel().getName().equals("Unknown")) 
-			{
-				for(int i = 0; i < devices.size();i++) //find our device by IP
-				{
-					d = devices.get(i);				
-					if (d.getIP().equals(CURR_NAC_IP)) //retrieve test device
-					{
-						break;
-					}
-				}
-				break;
-			}
-			try { Thread.sleep(3000); }
-			catch (InterruptedException e) { }
-		}
-		
 		//Un-set digital outputs
 		for(int i=1; i<=m.getDigitalOutputCount(); i++ ) {
 			assertEquals(true, d.getOutputState(i)); //confirm set successfully
@@ -180,8 +162,10 @@ class NacTest
 			try { Thread.sleep(500); }
 			catch (InterruptedException e) { }
 		}
+		//refresh device states
+		d = refreshDevice(as);
 		
-		//PA Controller 
+	//PA Controller testing
 		PAController pac = new PAController(); 
 		PaSourceArray paSourceList = new PaSourceArray();
 		PaSource paSource = new PaSource();
@@ -201,27 +185,23 @@ class NacTest
 		Gain sinkGain = new Gain(); 
 		sinkGain.setLevel(DEF_GAIN);
 		
-		//setup test announcement for playback on sinks
-		
-		
 		//Show all sources
 		for(int i=0; i < paSourceList.size(); i++) 
 		{
 			paSource = paSourceList.get(i);
-			if(Integer.toString(paSource.getId()).contains(dest)) 
+			if(Integer.toString(paSource.getId()).contains(dest)) //partial matching device sources
 			{
 				System.out.println("PA Source ID is: " + paSource.getId());
 				sourceCount++;
 			}
 		}
-		
 		assertEquals(sourceCount, CURR_DEV_SRC_COUNT);
 		
-		
+		//Show all sinks
 		for (int i=0; i < paSinkList.size(); i++) 
 		{
 			paSink = paSinkList.get(i);
-			if(Integer.toString(paSink.getId()).contains(dest)) 
+			if(Integer.toString(paSink.getId()).contains(dest)) //partial matching device sinks
 			{
 				System.out.println("PA Sink ID is: " + paSink.getId());
 				assertEquals(CURR_NAC_IP, paSink.getIpAddress()); //check IP of each sink against device IP
@@ -232,16 +212,75 @@ class NacTest
 				paSink.setOutputGain(sinkGain);
 				assertEquals(paSink.getGain().getLevel(), DEF_GAIN);
 				
-				
 			}
 		}
-		
-		
 		assertEquals(sinkCount, CURR_DEV_SNK_COUNT);
 		
 		
+	//Test PA on All sinks	
+		
+		//setup test announcement for playback on sinks
+		
+		int dictionaryId = 100000; //single dictionary item
+		NumberArray dvaItems = new NumberArray();
+		dvaItems.add(dictionaryId);
+		
+		StringArray outputZoneList = new StringArray();
+		StringArray outputVisualList = new StringArray();
+		
+		
+		//Testing zones
+		PaZoneArray paZoneList = new PaZoneArray();
+		PaZone zone = new PaZone(); 
+		
+		paZoneList = pac.getPaZones();
+		for (int i=0; i < paZoneList.size(); i++) 
+		{
+			zone = paZoneList.get(i);
+			if (zone.getId().contains("Test/Test"))
+			{
+				outputZoneList.add(zone.getId());
+			}
+		}
+		
+		//Play out to all relevant zones
+		pac.playMessage(outputZoneList, outputVisualList, sinkGain, dvaItems, null, false, true, 0, 0, 0);
+		
+		
+		
 		as.disconnect(); 
-		//fail("Not yet completed");
 	}
 
+	
+	
+	
+	
+	private Device refreshDevice (AudioServer as) 
+	{
+		DeviceStateArray devices = new DeviceStateArray();
+		Device d = new Device(); //test device
+		
+		while(true) 
+		{
+			devices = as.getDeviceStates();
+			if (devices.size() > 0)// && !devices.get(0).getDeviceModel().getName().equals("Unknown")) 
+			{
+				for(int i = 0; i < devices.size();i++) //find our device by IP
+				{
+					d = devices.get(i);				
+					if (d.getIP().equals(CURR_NAC_IP)) //retrieve test device
+					{
+						return d;
+					}
+				}
+				break;
+			}
+			try { Thread.sleep(3000); }
+			catch (InterruptedException e) { }
+		}
+		return null;
+	}
+	
+	
+	
 }
